3f0d3679f58e76a3822b6efc638c60797ccec4ed
Giannis Kotsias <johnkotsias@hotmail.gr>
2021-07-02 12:08:03 +0300

    update print messages

--- covidspreadingdays.c
+++ covidspreadingdays.c
@@ -9,7 +9,7 @@ int main(void){
       }
       printf("Info: R number is the spreading index. For example: For R=1.2 an infected person spreads to 1.2 people the disease(Covid-19) in the next day \n");
       printf("Please input the population number(integer) and the R number(float)\n");
-      printf("Example Input: 10000000 2. ==> population=10000 R_index=2(1 infected person infects 2 the next day)\n");
+      printf("Example Input: 10000000 1.78 ==> population=10000 R_index=1.78(1 infected person infects 1.78 the next day)\n");
 // input
       int population=0, days=1, infected=1;
       double rIndex=0;
@@ -21,8 +21,8 @@ int main(void){
             days++;
             infected += infected*rIndex;
             sumInfected+=infected;
-            printf("Day %d   ==>   %d people are infected from Covid-19 pandemic ------ percentage is %.3lf%% of new cases\n", days, (int)infected-1, (1-(infected)/sumInfected)*100);
+            printf("Day %d   ==>   %d people are infected from Covid-19 pandemic started from 1 infected person------ percentage is %.3lf%% of new cases\n", days, (int)infected-1, (1-(infected)/sumInfected)*100);
       }
-            printf("\nFinally,  %d days are needed for %d of population to be infected from Covid-19 disease with an R-index of %.2lf\n\n", days, population, rIndex);
+            printf("\nFinally,  %d days are needed for %d of population to be infected from 1 infected person with Covid-19 disease with an R-index of %.2lf\n\n", days, population, rIndex);
 	return 0;
 }
